8e28191226f1ac8ca132e14c5c2341883b1e4b7b
juanjo32 <juanjosegiraldo4@gmail.com>
2022-02-16 16:46:43 -0500

    Update Administrador.java

--- src/Restaurante/Administrador.java
+++ src/Restaurante/Administrador.java
@@ -59,5 +59,30 @@ public class Administrador {
 	public void NuevoPedido(String[] nombresPlatos, String nombreUsu, String direccionUsu, String telefonoUsu){
 		inti=0; //borrar
 	}
+	public void addPlato(String nombre,Ingredientes[] ingredientesTotal, double precio) {
+		if(buscarPlato(nombre) == -1) {
+			platosTotal= Arrays.copyOf(platosTotal,platosTotal.length+1);
+			platosTotal[platosTotal.length-1]= new Platos(nombre,ingredientesTotal,precio);
+		}
+	}
+	public void borrarPlato(String nombre) {
+		if(buscarPlato(nombre) != -1) {
+			int i=buscarPlato(nombre);
+			int j=buscarPlato(nombre)+1;
+			while(i<platosTotal.length) {
+			platosTotal[i]= platosTotal[j];
+			i++;j++;
+		}
+			platosTotal= Arrays.copyOf(platosTotal, platosTotal.length-1);
+			}
+	}
+	public int buscarPlato(String Nombre) {
+		String n= Nombre.toLowerCase();
+		int i=0;
+		while(i<platosTotal.length && !(platosTotal[i].getNombre().equals(n))) {
+			i++;
+		}
+		return(i<platosTotal.length)?i:-1;
+	}
 	
 }
