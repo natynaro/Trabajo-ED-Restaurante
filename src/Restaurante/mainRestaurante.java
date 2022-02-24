de27001a61d1dc3478c170be5c8f5eaa15194e42
Natalia Naranjo <90767304+natynaro@users.noreply.github.com>
2022-02-23 19:44:55 -0500

    main

--- src/Restaurante/mainRestaurante.java
+++ src/Restaurante/mainRestaurante.java
@@ -4,7 +4,14 @@ public class mainRestaurante {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 //probando
+		Pedidos[] pedidosTotal= new Pedidos[0];
+		Ingredientes[] ingredientesTotal= new Ingredientes[0];
+		Platos[] platosTotal= new Platos[0];
+		Domiciliario[] domiciliarios= new Domiciliario[0];
+		Administrador administrador = new Administrador(pedidosTotal, ingredientesTotal, platosTotal, domiciliarios); 
+		
 	}
 
 }
